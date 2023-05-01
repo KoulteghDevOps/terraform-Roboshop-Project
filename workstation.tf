cc0f2b5cb0348280def7c8585676e977dee2d853
KoulteghDevOps <koultegh@outlook.com>
2023-05-01 22:59:19 +0100

    tf file updated

--- workstation.tf
+++ workstation.tf
@@ -4,192 +4,224 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
 }
 
-resource "aws_instance" "frontend" {
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "components" {
+  default = [ "frontend", "mongodb", "catalogue" ]
+}
+  resource "aws_instance" "instance" {
+  count         = length(var.components)
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = "frontend"
+    Name = var.components[count.index]
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "frontend-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
+# resource "aws_instance" "frontend" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-output "frontend" {
-    value = aws_instance.frontend.public_ip
-}
+#   tags = {
+#     Name = "frontend"
+#   }
+# }
 
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+# resource "aws_route53_record" "frontend" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "frontend-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.frontend.private_ip]
+# }
 
-  tags = {
-    Name = "mongodb"
-  }
-}
+# output "frontend" {
+#     value = aws_instance.frontend.public_ip
+# }
 
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "mongodb-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+# resource "aws_instance" "mongodb" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "catalogue"
-  }
-}
+#   tags = {
+#     Name = "mongodb"
+#   }
+# }
 
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "catalogue-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "mongodb-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.mongodb.private_ip]
+# }
+# resource "aws_instance" "catalogue" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "catalogue"
+#   }
+# }
 
-  tags = {
-    Name = "redis"
-  }
-}
+# resource "aws_route53_record" "catalogue" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "catalogue-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.catalogue.private_ip]
+# }
 
-resource "aws_route53_record" "redis" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "redis-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
+# resource "aws_instance" "redis" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "user" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "redis"
+#   }
+# }
 
-  tags = {
-    Name = "user"
-  }
-}
+# resource "aws_route53_record" "redis" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "redis-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.redis.private_ip]
+# }
 
-resource "aws_route53_record" "user" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "user-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
+# resource "aws_instance" "user" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "cart" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "user"
+#   }
+# }
 
-  tags = {
-    Name = "cart"
-  }
-}
+# resource "aws_route53_record" "user" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "user-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.user.private_ip]
+# }
 
-resource "aws_route53_record" "cart" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "cart-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
+# resource "aws_instance" "cart" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "cart"
+#   }
+# }
 
-  tags = {
-    Name = "mysql"
-  }
-}
+# resource "aws_route53_record" "cart" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "cart-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.cart.private_ip]
+# }
 
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "mysql-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
+# resource "aws_instance" "mysql" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "mysql"
+#   }
+# }
 
-  tags = {
-    Name = "rabbitmq"
-  }
-}
+# resource "aws_route53_record" "mysql" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "mysql-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.mysql.private_ip]
+# }
 
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "rabbitmq-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
+# resource "aws_instance" "rabbitmq" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "payment" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "rabbitmq"
+#   }
+# }
 
-  tags = {
-    Name = "payment"
-  }
-}
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "rabbitmq-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.rabbitmq.private_ip]
+# }
 
-resource "aws_route53_record" "payment" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "payment-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
+# resource "aws_instance" "payment" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "dispatch" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "payment"
+#   }
+# }
 
-  tags = {
-    Name = "dispatch"
-  }
-}
+# resource "aws_route53_record" "payment" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "payment-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.payment.private_ip]
+# }
 
-resource "aws_route53_record" "dispatch" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "dispatch-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.dispatch.private_ip]
-}
+# resource "aws_instance" "dispatch" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-resource "aws_instance" "shipping" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+#   tags = {
+#     Name = "dispatch"
+#   }
+# }
 
-  tags = {
-    Name = "shipping"
-  }
-}
+# resource "aws_route53_record" "dispatch" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "dispatch-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.dispatch.private_ip]
+# }
 
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z09569901LP0VHA42NP6C"
-  name    = "shipping-dev.gilbraltar.co.uk"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
+# resource "aws_instance" "shipping" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
+#   tags = {
+#     Name = "shipping"
+#   }
+# }
+
+# resource "aws_route53_record" "shipping" {
+#   zone_id = "Z09569901LP0VHA42NP6C"
+#   name    = "shipping-dev.gilbraltar.co.uk"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.shipping.private_ip]
+# }
