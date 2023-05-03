84840c863c2ad736930da9144648c2911da42222
KoulteghDevOps <koultegh@outlook.com>
2023-05-03 17:29:28 +0100

    Snippet made dry

--- workstation.tf
+++ workstation.tf
@@ -1,64 +1,20 @@
-data "aws_ami" "centos" {
-  owners = ["973714476881"]
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-}
+# data "aws_ami" "centos" {
+#   owners = ["973714476881"]
+#   most_recent      = true
+#   name_regex       = "Centos-8-DevOps-Practice"
+# }
 
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
+# data "aws_security_group" "allow-all" {
+#   name = "allow-all"
+# }
 # variable "instance_type" {
 #   default = "t2.micro"
 # }
 
 variable "components" {
-  default = {
-    frontend = {
-      name = "frontend"
-      instance_type = "t2.micro"
-    }
-    mongodb = {
-      name = "mongodb"
-      instance_type = "t2.micro"
-    }
-    catalogue = {
-      name = "catalogue"
-      instance_type = "t2.micro"
-    }
-    redis = {
-      name = "redis"
-      instance_type = "t2.micro"
-    }
-    user = {
-      name = "user"
-      instance_type = "t2.micro"
-    }
-    cart = {
-      name = "cart"
-      instance_type = "t2.micro"
-    }
-    mysql = {
-      name = "mysql"
-      instance_type = "t2.micro"
-    }
-    rabbitmq = {
-      name = "rabbitmq"
-      instance_type = "t2.micro"
-    }
-    payment = {
-      name = "payment"
-      instance_type = "t2.micro"
-    }
-    shipping = {
-      name = "shipping"
-      instance_type = "t2.micro"
-    }
-    dispatch = {
-      name = "dispatch"
-      instance_type = "t2.micro"
-    }
-  }
+  default = {}
 }
+
 resource "aws_instance" "instance" {
   for_each = var.components
   ami           = data.aws_ami.centos.image_id
