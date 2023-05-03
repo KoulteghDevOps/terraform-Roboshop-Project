82af7acd915c3b7116704572c00870b43c87c891
KoulteghDevOps <koultegh@outlook.com>
2023-05-03 11:17:02 +0100

    Anothe update to the bk name

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "roboshopbk"
-        key = "roboshop/dev/terraform.tfstate"
+        key = "roboshopbk/dev/terraform.tfstate"
         region = "us-east-1"
     }
 }
