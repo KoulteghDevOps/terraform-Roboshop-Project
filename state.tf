6a9cd99d7e7563f5a08a7537588c0ba42d84338e
KoulteghDevOps <koultegh@outlook.com>
2023-05-03 12:35:14 +0100

    state file updated

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        bucket = "roboshopbk"
-        key = "roboshopbk/dev/terraform.tfstate"
+        bucket = "roboshop-project"
+        key = "roboshop/dev/terraform.tfstate"
         region = "us-east-1"
     }
 }
