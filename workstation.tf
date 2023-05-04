d67c0a717d0aa3de63a9e3a889a99c27a156fc13
KoulteghDevOps <koultegh@outlook.com>
2023-05-04 12:45:42 +0100

    provisioner updated

--- workstation.tf
+++ workstation.tf
@@ -22,6 +22,7 @@ resource "aws_instance" "instance" {
 }
 
 resource "null_resource" "provisioner" {
+
   depends_on   = [aws_instance.instance, aws_route53_record.records]
   for_each     = var.components
   provisioner "remote-exec" {
@@ -32,9 +33,10 @@ resource "null_resource" "provisioner" {
       password = "DevOps321"
       host     = aws_instance.instance[each.value["name"]].private_ip
     }
+
     inline = [
       "rm -rf roboshop-shell",
-      "git clone https://github.com/KoulteghDevOps/roboshop-shell.git",
+      "git clone https://github.com/KoulteghDevOps/roboshop-shell",
       "cd roboshop-shell",
       "sudo bash ${each.value["name"]}.sh"
     ]
