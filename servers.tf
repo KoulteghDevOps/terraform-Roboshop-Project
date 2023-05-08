c1ac5f8eb3e53f2ebf1dafb4ad8001516e887471
KoulteghDevOps <koultegh@outlook.com>
2023-05-08 21:17:25 +0100

    server file created from workstation

---
+++ servers.tf
@@ -0,0 +1,22 @@
+module "database-servers" {
+  for_each = var.database_servers
+
+  source         = "./module"
+  component_name = each.value["name"]
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
+  provisioner    = true
+}
+
+module "app-servers" {
+  depends_on = [ module.database-servers ]
+  for_each = var.app_servers
+
+  source         = "./module"
+  component_name = each.value["name"]
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
+}
+
